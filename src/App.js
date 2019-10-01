5d7be5d029eb7c7c9c0f1e90e591dbfa4d962400
eldenhowes <williameldenhowes@gmail.com>
2019-10-01 18:34:07 +0100

    Added Home button

--- src/App.js
+++ src/App.js
@@ -6,31 +6,33 @@ import { Link } from 'react-router-dom';
 
 
 function App() {
-  return (
-  <div className="demo-big-content">
-      <Layout>
-          <Header className="header-color" title="Title" scroll>
-              <Navigation>
-                  <a href="https://willhowes.herokuapp.com" target="_blank" rel="noopener noreferrer" >CV</a>
-                  <Link to="/aboutMe">About Me</Link>
-                  <Link to="/projects">Projects</Link>
-                  <Link to="/contact">Contact</Link>
-              </Navigation>
-          </Header>
-          <Drawer title="Title">
-              <Navigation>
-                  <a href="https://willhowes.herokuapp.com" target="_blank" rel="noopener noreferrer" >CV</a>
-                  <Link to="/aboutMe">About Me</Link>
-                  <Link to="/projects">Projects</Link>
-                  <Link to="/contact">Contact</Link>
-              </Navigation>
-          </Drawer>
-          <Content>
-              <div className="page-content" />
-              <Main></Main>
-          </Content>
-      </Layout>
-  </div>  
+	return (
+	<div className="demo-big-content">
+			<Layout>
+					<Header className="header-color" title="Will Howes - Portfolio" scroll>
+							<Navigation>
+									<Link to="/">Home</Link>
+									<a href="https://willhowes.herokuapp.com" target="_blank" rel="noopener noreferrer" >CV</a>
+									<Link to="/aboutMe">About Me</Link>
+									<Link to="/projects">Projects</Link>
+									<Link to="/contact">Contact</Link>
+							</Navigation>
+					</Header>
+					<Drawer title="Title">
+							<Navigation>
+									<Link to="/">Home</Link>
+									<a href="https://willhowes.herokuapp.com" target="_blank" rel="noopener noreferrer" >CV</a>
+									<Link to="/aboutMe">About Me</Link>
+									<Link to="/projects">Projects</Link>
+									<Link to="/contact">Contact</Link>
+							</Navigation>
+					</Drawer>
+					<Content>
+							<div className="page-content" />
+							<Main></Main>
+					</Content>
+			</Layout>
+	</div>  
 );
 }
 
