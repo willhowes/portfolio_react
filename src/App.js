364be7da7d773ff588b0d6584477402f77366487
eldenhowes <williameldenhowes@gmail.com>
2019-10-01 17:06:52 +0100

    landing page styled and social media links added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="demo-big-content">
+      <Layout>
+          <Header className="header-color" title="Title" scroll>
+              <Navigation>
+                  <a href="https://willhowes.herokuapp.com" target="_blank" rel="noopener noreferrer" >CV</a>
+                  <Link to="/aboutMe">About Me</Link>
+                  <Link to="/projects">Projects</Link>
+                  <Link to="/contact">Contact</Link>
+              </Navigation>
+          </Header>
+          <Drawer title="Title">
+              <Navigation>
+                  <a href="https://willhowes.herokuapp.com" target="_blank" rel="noopener noreferrer" >CV</a>
+                  <Link to="/aboutMe">About Me</Link>
+                  <Link to="/projects">Projects</Link>
+                  <Link to="/contact">Contact</Link>
+              </Navigation>
+          </Drawer>
+          <Content>
+              <div className="page-content" />
+              <Main></Main>
+          </Content>
+      </Layout>
+  </div>  
+);
 }
 
 export default App;
---
+++ src/components/landingPage.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl'
+
+class LandingPage extends Component {
+    render() {
+        return(
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img 
+                            src="https://icon-library.net/images/default-profile-icon/default-profile-icon-16.jpg"
+                            alt="avatar"
+                            className="avatar-img"
+                        />
+                        <div className="banner-text">
+                            <h1>Software Developer</h1>
+                            <hr/>
+                            <p>Ruby | Rails | Javascript</p>
+
+                            <div className="social-links">
+                            
+                                <a href="https://github.com/willhowes" target="_blank" rel="noopener noreferrer" >
+                                    <i className="fab fa-github-square" aria-hidden="true" />                               
+                                </a>
+
+                                <a href="https://www.linkedin.com/in/will-howes-12a9b529/" target="_blank" rel="noopener noreferrer" >
+                                    <i className="fab fa-linkedin" aria-hidden="true"/>                                
+                                </a>
+                                <a href="https://medium.com/@williameldenhowes" target="_blank" rel="noopener noreferrer" >
+                                    <i className="fab fa-medium" aria-hidden="true"/>                                
+                                </a>
+                            </div>
+
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
+            )
+    }
+}
+
+export default LandingPage;
